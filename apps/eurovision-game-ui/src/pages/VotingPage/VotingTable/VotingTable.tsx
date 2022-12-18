1021aa4053167ab4ea3aedae693d6079402930a5
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-12-18 20:55:03 +0200

    upd: add formik to form

--- apps/eurovision-game-ui/src/pages/VotingPage/VotingTable/VotingTable.tsx
+++ apps/eurovision-game-ui/src/pages/VotingPage/VotingTable/VotingTable.tsx
@@ -2,8 +2,8 @@ import { Grid } from "@mui/material";
 import { styles } from "./VotingTable.styles";
 import { useEffect, useState } from "react";
 import { Box } from "@mui/system";
-import VotingModal from "../VotingModal/VotingModal";
 import CountryCard from "../CountryCard/CountryCard";
+import { Formik } from "formik";
 
 interface ICountry {
 	country: string;
@@ -16,6 +16,7 @@ const VotingTable: React.FC = () => {
 	const [isLoading, setIsLoading] = useState(countries.length === 0);
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
+	// TODO: replace with RTK query
 	useEffect(() => {
 		fetch("http://localhost:3004/EV22")
 			.then((res) => res.json())
@@ -26,35 +27,42 @@ const VotingTable: React.FC = () => {
 
 	const toggleModal = (): void => setIsModalOpen(!isModalOpen);
 
-	return (
-		<>
-			{isModalOpen && (
-				<VotingModal isOpen={isModalOpen} handleClose={toggleModal} />
-			)}
-			{isLoading ? (
-				<Box>Loading</Box>
-			) : (
-				<Grid container sx={styles.tableContainer}>
-					{countries.map((entry: ICountry) => (
-						<Grid
-							item
-							md={4}
-							sm={6}
-							xs={12}
-							key={entry.country}
-							sx={styles.card}
-						>
-							<CountryCard
-								country={entry.country}
-								artist={entry.artist}
-								song={entry.song}
-								toggleModal={toggleModal}
-							/>
-						</Grid>
-					))}
-				</Grid>
-			)}
-		</>
+	const initialValues = countries.reduce(
+		(prev, { country, vote }) => ({
+			...prev,
+			[country]: vote,
+		}),
+		{}
+	);
+
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	const onSubmit = (): void => {};
+
+	return isLoading ? (
+		// TODO: replace with spinner or skeleton
+		<Box>Loading</Box>
+	) : (
+		<Formik initialValues={initialValues} onSubmit={onSubmit}>
+			<Grid container sx={styles.tableContainer}>
+				{countries.map((entry: ICountry) => (
+					<Grid
+						item
+						md={4}
+						sm={6}
+						xs={12}
+						key={entry.country}
+						sx={styles.card}
+					>
+						<CountryCard
+							country={entry.country}
+							artist={entry.artist}
+							song={entry.song}
+							toggleModal={toggleModal}
+						/>
+					</Grid>
+				))}
+			</Grid>
+		</Formik>
 	);
 };
 
