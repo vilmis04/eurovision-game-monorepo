7bfb962b64b2092c08f5cf53f500fb80cbf4ac87
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2024-03-05 22:47:13 +0200

    feat: add initial dashboard ui structure

--- apps/admin-portal/src/app/page.tsx
+++ apps/admin-portal/src/app/page.tsx
@@ -1,20 +1,22 @@
 import { Box } from '@mui/material';
-import { getIsAuthenticated } from './api/auth/authApi.server';
 import Login from './Login/Login';
 import { ClientProvider } from './providers/ClientProvider';
+import { Dashboard } from './Dashboard/Dashborad';
 
 const BASE_URL = process.env.BASE_SERVICE_URL;
 
 export default async function Index() {
-  const data = await getIsAuthenticated();
-  let isAuthenticated: boolean = false;
-  if (data.status === 200) {
-    isAuthenticated = await data.json();
-  }
+  // const data = await getIsAuthenticated();
+  // let isAuthenticated: boolean = false;
+  // if (data.status === 200) {
+  //   isAuthenticated = await data.json();
+  // }
 
+  // TODO: remove after testing
+  const isAuthenticated = true;
   return (
     <ClientProvider>
-      {isAuthenticated ? <Box>No data found</Box> : <Login />}
+      {isAuthenticated ? <Dashboard /> : <Login />}
     </ClientProvider>
   );
 }
