92ac795fddde62895b4c12e66bec2b1767c424d9
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2024-03-13 22:40:08 +0200

    feat: add minimal styling

--- apps/game-client/src/app/sign-up/page.tsx
+++ apps/game-client/src/app/sign-up/page.tsx
@@ -3,15 +3,27 @@ import { SignUp } from '../actions/signUp';
 
 const SignUpPage = async () => (
   <Box>
-    <Box component="form" action={SignUp}>
-      <TextField name="username" placeholder="Enter username" />
-      <TextField name="password" placeholder="Enter password" type="password" />
+    <Box
+      component="form"
+      action={SignUp}
+      className="container mx-auto flex flex-col max-w-md gap-3 pt-2"
+    >
+      <TextField name="username" placeholder="Enter username" required />
+      <TextField
+        name="password"
+        placeholder="Enter password"
+        type="password"
+        required
+      />
       <TextField
         name="repeatPassword"
         placeholder="Repeat password"
         type="password"
+        required
       />
-      <Button type="submit">Sign in</Button>
+      <Button variant="outlined" type="submit">
+        Sign up
+      </Button>
     </Box>
   </Box>
 );
